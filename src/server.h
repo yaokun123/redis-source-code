4ab4768b152a429b7807b29a42264d2041a1b711
姚坤 <yaok@pingansec.com>
2022-06-14 13:18:05 +0800

    set

--- src/server.h
+++ src/server.h
@@ -600,7 +600,7 @@ typedef struct redisObject {
  * OBJ_HASH     ->  OBJ_ENCODING_ZIPLIST ->  使用压缩列表实现的哈希对象  （键和值的长度都小于64 & 节点元素小于512） 可以通过配置文件配置
  * OBJ_HASH     ->  OBJ_ENCODING_HT     ->  使用字典实现的哈希对象
  *
- * OBJ_SET      ->  OBJ_ENCODING_INTSET ->  使用整数集合实现的集合对象
+ * OBJ_SET      ->  OBJ_ENCODING_INTSET ->  使用整数集合实现的集合对象（所有元素都是整数值 & 节点元素小于512）可以通过配置文件配置
  * OBJ_SET      ->  OBJ_ENCODING_HT     ->  使用字典实现的集合对象
  *
  * OBJ_ZSET     ->  BJ_ENCODING_ZIPLIST ->  使用压缩列表实现的有序集合对象
